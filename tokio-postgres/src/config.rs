0425322502276293e80c99560b4894cbe5a646f8
Stas Kelvich <stas.kelvich@gmail.com>
2023-05-23 11:32:41 +0300

    Add text protocol based query method (#14)
    Add query_raw_txt client method
    
    It takes all the extended protocol params as text and passes them to
    postgres to sort out types. With that we can avoid situations when
    postgres derived different type compared to what was passed in arguments.
    There is also propare_typed method, but since we receive data in text
    format anyway it makes more sense to avoid dealing with types in params.
    
    This way we also can save on roundtrip and send Parse+Bind+Describe+Execute
    right away without waiting for params description before Bind.
    
    Use text protocol for responses -- that allows to grab
    postgres-provided serializations for types.
    
    Catch command tag.
    
    Expose row buffer size and add `max_backend_message_size` option to
    prevent handling and storing in memory large messages from the backend.
    
    Co-authored-by: Arthur Petukhovsky <petuhovskiy@yandex.ru>

--- tokio-postgres/src/config.rs
+++ tokio-postgres/src/config.rs
@@ -228,6 +228,7 @@ pub struct Config {
     pub(crate) channel_binding: ChannelBinding,
     pub(crate) load_balance_hosts: LoadBalanceHosts,
     pub(crate) replication_mode: Option<ReplicationMode>,
+    pub(crate) max_backend_message_size: Option<usize>,
 }
 
 impl Default for Config {
@@ -263,6 +264,7 @@ impl Config {
             channel_binding: ChannelBinding::Prefer,
             load_balance_hosts: LoadBalanceHosts::Disable,
             replication_mode: None,
+            max_backend_message_size: None,
         }
     }
 
@@ -568,6 +570,17 @@ impl Config {
         self.replication_mode
     }
 
+    /// Set limit for backend messages size.
+    pub fn max_backend_message_size(&mut self, max_backend_message_size: usize) -> &mut Config {
+        self.max_backend_message_size = Some(max_backend_message_size);
+        self
+    }
+
+    /// Get limit for backend messages size.
+    pub fn get_max_backend_message_size(&self) -> Option<usize> {
+        self.max_backend_message_size
+    }
+
     fn param(&mut self, key: &str, value: &str) -> Result<(), Error> {
         match key {
             "user" => {
@@ -711,6 +724,14 @@ impl Config {
                 };
                 if let Some(mode) = mode {
                     self.replication_mode(mode);
+                }
+            }
+            "max_backend_message_size" => {
+                let limit = value.parse::<usize>().map_err(|_| {
+                    Error::config_parse(Box::new(InvalidValue("max_backend_message_size")))
+                })?;
+                if limit > 0 {
+                    self.max_backend_message_size(limit);
                 }
             }
             key => {
